0e21a46aeed29f94b109e1e79377af3990f648dc
joe95-ux <ogorktabi@gmail.com>
2023-02-22 10:49:14 +0100

    fix configurations

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
-    <script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+  <script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div class="container-fluid">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
       <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
-        <div id="navbarColor01" class="collapse navbar-collapse">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item"><a class="nav-link active" href="<?php echo base_url(); ?>">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>about">About</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories">Categories</a></li>
-          </ul>
-          
-        </div>
+      <div id="navbarColor01" class="collapse navbar-collapse">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item"><a class="nav-link active" href="<?php echo base_url(); ?>">Home</a></li>
+          <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>about">About</a></li>
+          <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a></li>
+          <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories">Categories</a></li>
+        </ul>
+
       </div>
-    </nav>
-    
+    </div>
+  </nav>
+  <div class="container">
